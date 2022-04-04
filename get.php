e4d5c008a92b95c959a717c219136dbd45326a56
Your Name <Jamelehasaad@gmail.com>
2022-04-04 18:47:37 +0300

    first commit

---
+++ get.php
@@ -0,0 +1,29 @@
+<?php
+$servername = "localhost:3306";
+$username = "root";
+$password = "Asaad123!@#";
+$dbname = "sql_store";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+
+$sql = "SELECT customer_id,first_name,last_name FROM customers";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["customer_id"]. " - Name: " . $row["first_name"]. " " . $row["last_name"]. "<br>";
+  }
+}
+
+ else {
+  echo "0 results";
+}
+$conn->close();
+?>
